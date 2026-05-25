bad4063818b5cdd68cee6db6759e0d068beb4ce5
Hing-Liāng Kòo <hlku@cs.nctu.edu.tw>
2026-05-26 07:21:14 +0800

    add comments

--- src/alphabeta.py
+++ src/alphabeta.py
@@ -5,6 +5,7 @@ import board
 logging.basicConfig(level=logging.INFO, format = "%(asctime)s %(filename)s %(levelname)s:%(message)s")
 class AlphaBeta:
     def __init__(self, settings, board) -> None:
+        """Initialize the AlphaBeta engine based on the settings."""
         self.__settings = settings
         self.__board = board
         self.__limit = 3 + self.__settings.getConfig('level')['ab'] * 2
@@ -15,38 +16,55 @@ class AlphaBeta:
             self.__log.debug('Debug mode is on')
     
     def calculate(self):
-        solutions = set([9])
-        value = -100
-
-        for nx in self.__board.expand() :
-            v = self.__findMin(nx, 1, value)
-            if value == v :
+        """Calculate the best next step using the Alpha–Beta Pruning Algorithm."""
+        solutions = set() #positions of all best next steps 
+        value = -100 #value of a step, the higher the better for the computer
+        for nx in self.__board.expand() : #every possible postion for the next step
+            v = self.__findMin(nx, 1, value) #get the value of this step
+            if value == v : #same value to old solutions, add to set
                 solutions.add(nx.index(max(nx)))
-            elif value < v :
+            elif value < v : #find a better solution, clear old solutions and add new one
                 solutions = set([nx.index(max(nx))])
                 value = v
 
         self.__log.debug("Solutions: %s, Value: %d", str(solutions), value)
-        return list(solutions)[random.randint(0, len(solutions) - 1)]
+        #randomly choose one of the best solutions if there are multiple ones
+        return list(solutions)[random.randint(0, len(solutions) - 1)] 
 
     def __findMin(self, tsumeru, depth, MAX):
-        value = 100
-        if board.checkWin(tsumeru) : return 99 - depth
-        elif depth >= self.__limit : return 0
+        """Find the minimum value of the next step
+           which is best for user but worst for computer.
+           tsumeru: the pending solving board state after computer's step
+           depth: the current depth of the search tree
+           MAX: the current best value of computer's possible next steps"""
+        if board.checkWin(tsumeru) : return 99 - depth #computer win, the sooner the better
+        elif depth >= self.__limit : return 0 #reach the depth limit, no best solution found
         
-        for nx in board.expandBoard(tsumeru) :
+        value = 100
+        for nx in board.expandBoard(tsumeru) : #every possible postion for user's next step
+            #get the lowest value of this step, which is the best for user but worst for computer
             value = min(value, self.__findMax(nx, depth + 1, value))
-            if value < MAX : return value
+            #the user will get better value than computer's current best value (MAX)
+            #so computer will never choose this step (return immediately without searching deeper)
+            if value < MAX : return value 
             
         return value
         
     def __findMax(self, tsumeru, depth, MIN) :
-        value = -100
-        if board.checkWin(tsumeru) : return -99 + depth
-        elif depth >= self.__limit : return 0
+        """Find the maximum value of the next step
+           which is best for computer but worst for user.
+           tsumeru: the pending solving board state after user's step
+           depth: the current depth of the search tree
+           MIN: the current best value of user's possible next steps"""
+        if board.checkWin(tsumeru) : return -99 + depth #user win, the sooner the worse
+        elif depth >= self.__limit : return 0 #reach the depth limit, no best solution found
         
-        for nx in board.expandBoard(tsumeru) :
+        value = -100
+        for nx in board.expandBoard(tsumeru) : #every possible postion for computer's next step
+            #get the highest value of this step, which is the best for computer but worst for user
             value = max(value, self.__findMin(nx, depth + 1, value))
+            #the computer will get better value than user's current best value (MIN)
+            #so user will never choose this step (return immediately without searching deeper)
             if value > MIN : return value
             
         return value
