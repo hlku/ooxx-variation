5720fdb611ad067cf7dc11bd0d08b1e410adcc34
Hing-Liāng Kòo <hlku@cs.nctu.edu.tw>
2026-05-27 03:30:30 +0800

    add comments

--- game.py
+++ game.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import sys
 import src.ooxx, src.watcher
 
 if __name__ == '__main__':
-    watcher = src.watcher.Watcher()
+    watcher = src.watcher.Watcher() #a watcher to catch keyboard interrupt
     watcher.start()
 
     ooxx = src.ooxx.OOXX()
-    while ooxx.start(): pass
+    while ooxx.start(): pass #keep playing until player says no or watcher kills the process
